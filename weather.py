89fbf5d8d5aca4627f0f02e85613275a10081436
Jayanth <jayanthkumar597@gmail.com>
2022-05-10 19:46:31 +0530

    Updates files

---
+++ weather.py
@@ -0,0 +1,26 @@
+from typing import Container
+import requests
+import html
+#from browser import document, html 
+#container = document['container']
+
+input = 'Cheyyar'
+
+#def getweather(event):
+ #   if event.key == 'Enter':
+
+url = "https://weatherapi-com.p.rapidapi.com/current.json"
+
+querystring = {"q": input}
+
+headers = {
+	"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+	"X-RapidAPI-Key": "d0553e1541msh143a6ab1560e91ep17832cjsn99f8fe81fd86"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+
+#container <= input
+
+print(response.text)
