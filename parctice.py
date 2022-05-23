f357f28c3a3e0051b9ad54d627831237ede985fd
Jayanth <jayanthkumar597@gmail.com>
2022-05-23 15:22:35 +0530

    Updated

--- parctice.py
+++
@@ -1,6 +0,0 @@
-from masterclass import * 
-
-ob2 = Location()
-lat,long = ob2.get_locations()
-print('{} Latitude '.format(lat))
-print("{} Longitude ".format(long))
--- table.sql
+++
@@ -1,4 +0,0 @@
-Create a table tab  
-
-Test
-Test
