c4b207bdf1ea66c0ee0204aa17577f796fa733c1
Jayanth Kumar <jayanthkumar597@gmail.com>
2022-08-16 20:53:21 +0530

    java

--- JAVA/firstcode.java
+++ JAVA/firstcode.java
@@ -1,5 +1,7 @@
-class HelloWorld {
+public class Solution {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
