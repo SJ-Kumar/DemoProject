6ce764e946805732ef711b8c36a2f39cd62ca03a
Jayanth <jayanthkumar597@gmail.com>
2022-08-20 22:11:53 +0530

    url updated

--- python_practice/datasetload.py
+++ python_practice/datasetload.py
@@ -3,7 +3,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'C:\10-Practice\DemoProject\ds_salaries.csv')
+#df = pd.read_csv(r'C:\10-Practice\DemoProject\ds_salaries.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/SJ-Kumar/datasets/main/ds_salaries.csv?token=GHSAT0AAAAAABWK2JBPHZOSPY6DMP4MUCFYYYBB23Q')
 print(df.head(10))
 print(df.shape)
 print(df.job_title.value_counts())
--- python_practice/outlier.py
+++ python_practice/outlier.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 try:
-    url = 'https://raw.githubusercontent.com/SJ-Kumar/datasets/main/bank_marketing_dataset.csv?token=GHSAT0AAAAAABWK2JBOOD2QVORNP6SNJTSSYYBBO2Q'
+    url = 'https://raw.githubusercontent.com/SJ-Kumar/datasets/main/aqi.csv?token=GHSAT0AAAAAABWK2JBOOYPG7YZ4TYWJSGGGYYBBXUA'
 
     df = pd.read_csv(url)
     print(df.describe())
