5ac26ab8ecf3fc70e152dc943636208ceabd6f57
Jayanth <jayanthkumar597@gmail.com>
2022-08-28 19:51:13 +0530

    Foldercleanup

--- Demos_fullfiles/mysite/main/views.py
+++
@@ -1,6 +0,0 @@
-from mysite.main.models import Tutorial
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def homepage (request):
-    return HttpResponse("Wow this is response from <strong>views</strong> page!")
